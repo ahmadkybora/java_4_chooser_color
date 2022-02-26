4d1fe0b4847d676b23cfde7e350b10f31cad844f
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-02-26 17:51:02 +0330

    =new

---
+++ src/MyFrame.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+    JLabel label;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Pick a Color");
+        button.addActionListener(this);
+
+        label = new JLabel();
+        label.setBackground(Color.white);
+        label.setText("this is some text");
+        label.setFont(new Font("mm", Font.PLAIN, 100));
+        label.setOpaque(true);
+
+        this.add(button);
+        this.add(label);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button){
+            JColorChooser colorChooser = new JColorChooser();
+            Color color = JColorChooser.showDialog(null, "asd", Color.black);
+
+//            label.setForeground(color);
+            label.setBackground(color);
+        }
+    }
+}
